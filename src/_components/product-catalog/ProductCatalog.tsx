1f23794281523a238aa5d78154122a8cf810d142
amanandhar <adam.manandhar@gmail.com>
2023-07-01 19:27:44 -0500

    Updated Product Catalog.

--- src/_components/product-catalog/ProductCatalog.tsx
+++ src/_components/product-catalog/ProductCatalog.tsx
@@ -161,71 +161,74 @@ export const ProductCatalog = (props: IProductCatalogProps) => {
     <section>
       <div className="container px-4 px-lg-5 mt-5">
         <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-          {products.map((product, index) => (
-            <div key={index} className="col mb-5">
-              <div className="card h-100">
-                {product.imageName ? (
-                  <img
-                    className="card-img-top"
-                    src={
-                      process.env.REACT_APP_STATIC_URL + "/" + product.imageName
-                    }
-                    alt={product.name}
-                  />
-                ) : (
-                  <img
-                    className="card-img-top"
-                    src={
-                      process.env.PUBLIC_URL +
-                      "/images/dummy/product-image-placeholder.jpg"
-                    }
-                    alt={product.name}
-                  />
-                )}
+          {products.length > 0 &&
+            products.map((product, index) => (
+              <div key={index} className="col mb-5">
+                <div className="card h-100">
+                  {product.imageName ? (
+                    <img
+                      className="card-img-top"
+                      src={
+                        process.env.REACT_APP_STATIC_URL +
+                        "/" +
+                        product.imageName
+                      }
+                      alt={product.name}
+                    />
+                  ) : (
+                    <img
+                      className="card-img-top"
+                      src={
+                        process.env.PUBLIC_URL +
+                        "/images/dummy/product-image-placeholder.jpg"
+                      }
+                      alt={product.name}
+                    />
+                  )}
 
-                <div className="card-body p-4">
-                  <div className="text-center">
-                    <h5 className="fw-bolder">{product.name}</h5>
-                    <h6>
-                      {product.code}
-                      {product.batchNumber !== 0
-                        ? "." + product.batchNumber
-                        : ""}
-                      {product.subCode !== 0 ? "." + product.subCode : ""}
-                    </h6>
-                    <div>
-                      {product.customizedQuantity} {product.customizedUnit}
-                      {": Rs. " + product.price}
+                  <div className="card-body p-4">
+                    <div className="text-center">
+                      <h5 className="fw-bolder">{product.name}</h5>
+                      <h6>
+                        {product.code}
+                        {product.batchNumber !== 0
+                          ? "." + product.batchNumber
+                          : ""}
+                        {product.subCode !== 0 ? "." + product.subCode : ""}
+                      </h6>
+                      <div>
+                        {product.customizedQuantity} {product.customizedUnit}
+                        {": Rs. " + product.price}
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                  <div className="text-center">
-                    <div className="btn mt-auto">
-                      {product.isButtonEnabled && (
-                        <UpdateQuantityButton
-                          onClick={(value) =>
-                            handleChangeQuantityButtonClick(
-                              product.id || 0,
-                              value
-                            )
-                          }
-                        />
-                      )}
+                  <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                    <div className="text-center">
+                      <div className="btn mt-auto">
+                        {product.isButtonEnabled && (
+                          <UpdateQuantityButton
+                            onClick={(value) =>
+                              handleChangeQuantityButtonClick(
+                                product.id || 0,
+                                value
+                              )
+                            }
+                          />
+                        )}
 
-                      {!product.isButtonEnabled && (
-                        <AddCartButton
-                          onClick={() =>
-                            handleAddCartButtonClick(product.id || 0, 1)
-                          }
-                        />
-                      )}
+                        {!product.isButtonEnabled && (
+                          <AddCartButton
+                            onClick={() =>
+                              handleAddCartButtonClick(product.id || 0, 1)
+                            }
+                          />
+                        )}
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
       <div
